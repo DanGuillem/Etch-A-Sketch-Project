2ebc59e5f43ab97855da9d71db65749c1fb37069
DanGuillem <daniel.guillem@hotmail.co.uk>
2023-05-10 16:17:27 +0100

    Full Logic completed

--- javascript.js
+++ javascript.js
@@ -1,15 +1,59 @@
-// Make a 16 x 16 grid by appending rows to container and appending cells to row
-for (let i = 0; i < 16; i++){
+// Default - Make a 16 x 16 grid by appending rows to container and appending cells to row
+
+function createGrid(size) {
     const container = document.querySelector('div');
+    for (let i = 0; i < size; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+    
+        for (let i = 0; i < size; i++) {
+            const div = document.createElement('div');
+            div.classList.add('cell')
+            row.appendChild(div);
+        }
+    }
 
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
+    cells = document.querySelectorAll('.cell');
+    cells.forEach((div) => {
+    
+        div.addEventListener('mouseover', () => {
+            div.classList.add('mousedover')
+        });
+});
+}
 
-    for (let i = 0; i < 16; i++) {
-        const div = document.createElement('div');
-        div.classList.add('cell')
-        row.appendChild(div);
+function removeAllChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
 
+function resetGrid() {
+
+    gridSize = prompt("How large would you like your grid? (Maximum 100, minimum 16)");
+
+    if (gridSize <= 100 && gridSize >= 16){
+        removeAllChildren(container);
+        createGrid(gridSize);
+    }
+    else {
+        prompt("Please enter a valid grid size")
+    }
+    
+}
+
+resetGridButton = document.querySelector('button');
+resetGridButton.addEventListener('click', resetGrid);
+
+createGrid(16);
+
+// Adds class called 'mousedover' which changes cell color
+
+cells = document.querySelectorAll('.cell');
+cells.forEach((div) => {
+    
+        div.addEventListener('mouseover', () => {
+            div.classList.add('mousedover')
+        });
+});
